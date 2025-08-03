7223295f282298376405c5a009d95931c6f65ac4
PaulSolano <psolanot8@gmail.com>
2025-08-03 18:32:58 -0500

    primer commit

---
+++ src/main/java/org/example/Dashboard.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import javax.swing.*;
+
+public class Dashboard {
+    private JPanel PanelPrincipal;
+    private JPanel PanelNavegacion;
+    private JPanel PanelContDinamico;
+    private JButton salirButton;
+    private JButton clientesButton;
+    private JButton reportesButton;
+    private JButton ventasButton;
+    private JButton gestionDeProductosButton;
+    private JLabel Logo;
+    private JPanel panelTitulo;
+    private JPanel panelResumenTarjetas;
+    private JPanel panelActividadReciente;
+}
---
+++ src/main/java/org/example/Login.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import javax.swing.*;
+
+public class Login {
+    private JPanel panelConten;
+    private JPanel PanelIzquierdo;
+    private JPanel PanelDerecho;
+    private JLabel txtUsuario;
+    private JTextField InputTxtUsuario;
+    private JLabel txtContraseña;
+    private JPasswordField InputTxtPassword;
+    private JButton ENTRARButton;
+}
